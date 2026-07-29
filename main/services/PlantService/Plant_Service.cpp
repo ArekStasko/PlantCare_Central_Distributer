5891ea6546f1710b344130b92aad59aee58201eb
ArekStasko <arek.stasko@gmail.com>
2026-07-29 22:39:12 +0200

    PLant service

---
+++ main/services/PlantService/Plant_Service.cpp
@@ -0,0 +1,5 @@
+//
+// Created by arekstasko on 7/29/26.
+//
+
+#include "Plant_Service.h"
---
+++ main/services/PlantService/Plant_Service.h
@@ -0,0 +1,16 @@
+//
+// Created by arekstasko on 7/29/26.
+//
+
+#ifndef PLANT_SERVICE_H
+#define PLANT_SERVICE_H
+
+
+
+class Plant_Service {
+
+};
+
+
+
+#endif //PLANT_SERVICE_H
